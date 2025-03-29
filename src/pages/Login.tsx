1276ea1113fbeef491ba03eeffc2fac00069a9b5
Master Quanh <131639169+GODKEYSS@users.noreply.github.com>
2025-03-29 10:40:20 +0700

    edit login page

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,8 @@
 import { Button, Checkbox, Form, Input } from 'antd';
 import { Link } from 'react-router';
 import Background from '../assets/images/Background.jpg';
+import FbIcon from '../components/Icons/FbIcon';
+import GoogleIcon from '../components/Icons/GoogleIcon';
 
 const Login = () => {
   type FieldType = {
@@ -12,35 +14,32 @@ const Login = () => {
 
 
   return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-4">
-      <div className="flex flex-col md:flex-row bg-white rounded-2xl shadow-xl overflow-hidden max-w-7xl w-full h-[96vh]">
-        {/* Form Section */}
+    <div className="min-h-screen flex flex-col md:flex-row">
+      {/* Form Section */}
+      <div className="w-full md:w-1/2 flex items-center justify-center p-8  bg-gradient-to-r from-violet-200 to-violet-400">
         <Form
-
           name="login"
           initialValues={{ remember: true }}
           autoComplete="off"
-          className="w-full md:w-1/2 p-12 space-y-6 flex flex-col justify-center"
+          className="w-full max-w-lg"
           layout="vertical"
         >
           <h1 className="text-center mb-6 text-xl font-semibold text-gray-700">
-            Vui lòng nhập thông tin để truy cập vào tài khoản!
+            Nhập đầy đủ thông tin để truy cập vào tài khoản!
           </h1>
 
           <Form.Item<FieldType>
-            label="Username"
+            label={<span className="text-gray-700">Username</span>}
             name="username"
-
             rules={[{ required: true, message: 'Vui lòng nhập tên đăng nhập!' }]}
           >
-            <Input tabIndex={1} placeholder="Tên đăng nhập" size="large" />
+            <Input placeholder="Tên đăng nhập" size="large" />
           </Form.Item>
 
           <Form.Item<FieldType>
+            label={<span className="text-gray-700">Password</span>}
             name="password"
-            label="Password"
             rules={[{ required: true, message: 'Vui lòng nhập mật khẩu!' }]}
-            style={{ marginBottom: "2rem" }}
           >
             <Input.Password placeholder="Mật khẩu" size="large" />
           </Form.Item>
@@ -51,10 +50,10 @@ const Login = () => {
               valuePropName="checked"
               noStyle
             >
-              <Checkbox>Ghi nhớ tôi</Checkbox>
+              <Checkbox>Nhớ mật khẩu</Checkbox>
             </Form.Item>
-            <Link to="#" className="text-blue-600 hover:text-blue-500">
-              Quên mật khẩu?
+            <Link to="#">
+              <p className="text-blue-500 hover:text-violet-600">Quên mật khẩu?</p>
             </Link>
           </div>
 
@@ -71,35 +70,35 @@ const Login = () => {
 
           <div className="flex items-center justify-center my-6">
             <div className="flex-grow h-px bg-gray-300" />
-            <span className="px-4 text-gray-500">Hoặc</span>
+            <span className="px-4 text-gray-700">Or</span>
             <div className="flex-grow h-px bg-gray-300" />
           </div>
 
           <div className="grid grid-cols-2 gap-4">
-            <Button size="large">
-              Đăng nhập bằng Google
+            <Button size="large" className="border-gray-300">
+              <GoogleIcon /> Đăng nhập bằng Google
             </Button>
             <Button size="large" className="border-gray-300">
-              Đăng nhập bằng Facebook
+              <FbIcon />Đăng nhập bằng Facebook
             </Button>
           </div>
 
-          <div className="text-center mt-6">
-            <span className="text-gray-600">Chưa có tài khoản? </span>
-            <Link to="/signup" className="text-blue-500 hover:text-blue-700">
-              Đăng ký
+          <div className="text-center mt-6 flex items-center justify-center">
+            <span className="text-gray-700 text-[16px]">Chưa có tài khoản?</span>
+            <Link to="/signup" >
+              <p className="text-blue-500 hover:text-violet-600 text-[16px] ml-1">Đăng ký</p>
             </Link>
           </div>
         </Form>
+      </div>
 
-        {/* Image Section */}
-        <div className="hidden md:block w-1/2 p-12">
-          <img
-            src={Background}
-            alt="Login Background"
-            className="w-full h-full object-cover rounded-lg"
-          />
-        </div>
+      {/* Image Section */}
+      <div className="hidden md:block w-full md:w-1/2">
+        <img
+          src={Background}
+          alt="Login Background"
+          className="w-full h-full object-cover"
+        />
       </div>
     </div>
   );
